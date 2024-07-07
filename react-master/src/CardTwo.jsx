edf4513026a56bb84de062e519c1f2c4cd6b547c
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-07 09:04:14 +0530

    Update CardTwo.jsx
    Implemented event handling in react.js

--- react-master/src/CardTwo.jsx
+++ react-master/src/CardTwo.jsx
@@ -27,16 +27,18 @@ const CardTwo = () => {
       isAvailable : false
     }
   ]
-  
+  const handleClick = () => {
+    alert();
+  }
   
   return (
-    <div className='w-full h-screen bg-zinc-300 flex flex-col justify-center  items-center gap-4'>
+    <div className='w-full h-screen bg-zinc-800 flex flex-col justify-center  items-center gap-4'>
      {data.map((elem,index)=>{
       return(
         <div key={index} className=' w-80 px-3 py-2 bg-zinc-100 rounded-md'>
-        <h3 className='font-semibold text-xl'>{elem.name}</h3>
+        <h3 className='font-semibold text-xl border-b-4'>{elem.name}</h3>
         <p className='text-xs mt-2'>{elem.desc}</p>
-        <button className={`px-2 py-1  text-xs font-semibold ${elem.isAvailable ? 'bg-blue-400':'bg-gray-300'} ${
+        <button onClick={handleClick} className={`px-2 py-1  text-xs font-semibold ${elem.isAvailable ? 'bg-blue-400':'bg-gray-300'} ${
         elem.isAvailable ? 'text-zinc-100':'text-black'} rounded mt-3`}>{elem.isAvailable ? "Listen Now" : "Not Available" }</button>
       </div>
       );
