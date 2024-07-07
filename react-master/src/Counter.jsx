aaef36b6ab61d5cc87f789f3d7b16bbfa6a1732f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-07 09:34:16 +0530

    Update Counter.jsx
    Implemented short-hand technique using arrow functions

--- react-master/src/Counter.jsx
+++ react-master/src/Counter.jsx
@@ -3,22 +3,33 @@ import React, { useState } from 'react'
 
 const Counter = () => {
 let[count, updateCount] = useState(0);
-const Increment =() => {
-  updateCount(count + 1);
-} 
-const Decrement =() => {
-  if(count > 0){
-    updateCount(count - 1);
-  }else{
-    count = 0;
-  }
-} 
+
+
+// const Increment =() => {
+//   updateCount(count + 1);
+// } 
+// const Decrement =() => {
+//   if(count > 0){
+//     updateCount(count - 1);
+//   }else{
+//     count = 0;
+//   }
+// } 
 return (
     <div className='w-full h-screen bg-zinc-500 flex justify-center items-center flex-col'>
       <h1 className='text-7xl text-white'>{count}</h1>
       <div className='flex justify-center items-center gap-5 mt-10'>
-        <button onClick={Increment} className='px-6 py-2 bg-red-600 rounded-full text-white text-2xl '>Increment</button>
-        <button onClick={Decrement} className='px-6 py-2 bg-blue-600 rounded-full text-white text-2xl '>Decrement</button>
+        <button onClick={()=>{updateCount(count+1)}} className='px-6 py-2 bg-red-600 rounded-full text-white text-2xl '>Increment</button>
+        <button onClick={()=>{
+          if(count > 0){
+            updateCount(count - 1);
+          }else{
+            count = 0;
+          }
+        }} 
+        
+        
+        className='px-6 py-2 bg-blue-600 rounded-full text-white text-2xl '>Decrement</button>
       </div>
     </div>
   )
